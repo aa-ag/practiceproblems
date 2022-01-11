f812f4e6efcae4cb45056c796a22e35cda9335fb
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-10 18:15:27 -0600

    added another solution which builds a hash table with the count and then searches for unique value in the count;  also passing all tests

--- solutions/strings/firstunique.py
+++ solutions/strings/firstunique.py
@@ -1,6 +1,10 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        for i in range(0, len(s)):
-            if s.count(s[i]) == 1:
-                return i
+        import collections
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx     
         return -1

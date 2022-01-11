05cd10f3eb8130d5a049fac3980282077f7256aa
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-10 18:18:36 -0600

    added docstring with proposed algo

---
+++ solutions/strings/validanagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+         decompose string into sorted characters
+         compare if they are the same
+        '''
+        pass

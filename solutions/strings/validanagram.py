ab005ae89ae2f51bea08e4ea9dec2008f51998e8
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-10 18:23:48 -0600

    added big o notation in docstring

--- solutions/strings/validanagram.py
+++ solutions/strings/validanagram.py
@@ -1,3 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        '''
+         O(nlogn) time: sorting costs + comparing sorted strings
+         O(1) space: Space depends on the sorting implementation which, usually, 
+          costs O(1)O(1) auxiliary space if heapsort is used
+        '''
         return sorted(s) == sorted(t)

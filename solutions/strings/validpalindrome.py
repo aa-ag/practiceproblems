45c7c08edc0c38d353f13dc980b78cc147ce7ee5
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-11 18:21:53 -0600

    added three test cases from problem

--- solutions/strings/validpalindrome.py
+++ solutions/strings/validpalindrome.py
@@ -1,7 +1,13 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        '''
-         (1) remove all non-alphanumeric characters
-         (2) check if s == s.reversed()
-         (3) if s is a white space, return true
-        '''
+def isPalindrome(self, s: str) -> bool:
+    '''
+        (1) remove all non-alphanumeric characters
+        (2) check if s == s.reversed()
+        (3) if s is a white space, return true
+    '''
+    import re
+    clean = re.sub('W+', '', s)
+    print(clean)
+
+isPalindrome("A man, a plan, a canal: Panama")
+isPalindrome("race a car")
+isPalindrome(" ")

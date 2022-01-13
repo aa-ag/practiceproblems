d786df928e439c86a992d5046b54565dc5d0a205
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-12 20:47:48 -0600

    added second step to docstring and bifurcated s into positive or negative

--- solutions/strings/stoint.py
+++ solutions/strings/stoint.py
@@ -1,7 +1,13 @@
 def myAtoi(s):
     '''
-     Read in and ignore any leading whitespace.
+     - Read in and ignore any leading whitespace.
+     - Check if the next character (if not already at the end of the string) 
+        is '-' or '+'. Read this character in if it is either. 
     '''
-    print(s.lstrip(' '))
+    s = s.lstrip(' ')
+    if s.startswith('-'):
+        print("negative")
+    else:
+        print("positive")
 
 myAtoi("   -42")

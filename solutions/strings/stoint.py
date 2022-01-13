76549261f1055f7d75373ec46b61cf31c1d9670a
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-12 20:58:45 -0600

    looped thru remaninig characters and break when next character isn't a number per algo flor provided in exercise

--- solutions/strings/stoint.py
+++ solutions/strings/stoint.py
@@ -16,7 +16,15 @@ def myAtoi(s):
         is_negative = False
 
     s = s.replace('-', '')
-    print(s)
+    
+    nums = ''
+
+    for i in s:
+        if i not in '0123456789':
+            break
+        else:
+            nums += i
+    print(nums)
 
 
 myAtoi("   -42")

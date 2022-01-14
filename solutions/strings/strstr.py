a51aad7f8543b36eb8aadab18d569def0717904d
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-14 17:36:46 -0600

    added linebreak in comments for test cases for readability

--- solutions/strings/strstr.py
+++ solutions/strings/strstr.py
@@ -2,14 +2,17 @@ def strStr(h: str, n: str) -> int:
     print(h, n)
 
 
-# Input: haystack = "hello", needle = "ll"
+# Input: haystack = "hello", 
+# needle = "ll"
 # Output: 2
 strStr("hello", "ll")
 
-# Input: haystack = "aaaaa", needle = "bba"
+# Input: haystack = "aaaaa", 
+# needle = "bba"
 # Output: -1
 strStr("aaaaa", "bba")
 
-# Input: haystack = "", needle = ""
+# Input: haystack = "", 
+# needle = ""
 # Output: 0
 strStr("", "")

4302e15050ebebe41d88e768e3608c20ded5dd0b
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-14 17:36:05 -0600

    added base function from leetcode and three base tests

---
+++ solutions/strings/strstr.py
@@ -0,0 +1,15 @@
+def strStr(h: str, n: str) -> int:
+    print(h, n)
+
+
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+strStr("hello", "ll")
+
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+strStr("aaaaa", "bba")
+
+# Input: haystack = "", needle = ""
+# Output: 0
+strStr("", "")

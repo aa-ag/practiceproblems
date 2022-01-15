0ae5db93c35ce303b1c8f79421f48ea43fb8f504
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-15 11:44:56 -0600

    added entire functioning algo for test case provided in leetcode;  yet to test other test cases;  workingo for now

--- easy/strings/countsay.py
+++ easy/strings/countsay.py
@@ -1,30 +1,33 @@
+from collections import Counter
+
 nums = {
     '1': 'one','2': 'two','3': 'three','4': 'four','5': 'five',
     '6': 'six','7': 'seven','8': 'eight','9': 'nine','0': 'ten',
 }
 
-def countAndSay(n: int) -> str:
-    # a_value = nums[n]
-    # a_key = list(nums.keys())[list(nums.values()).index(nums[n])]
-    
+def countAndSay(n: int) -> str:    
     i = 1
-    count = ''
+    count = str(i)
     say = ''
-    while i < n + 1:
-        count = str(i)
+    while i < n:
+        print(f"--->{i}")
         print(count)
         
-        say = f"{nums[count]} {count}" 
-        print(say)
+        current_count = Counter(count)
+        print(current_count)
 
-        new_count = list(nums.keys())[list(nums.values()).index(nums[count])]
-        new_say = count + new_count
-        print(new_say)
+        for k, v in current_count.items():
+            say = f"{nums[str(v)]} {k}" 
+            print(say)
+
+        count = count + list(nums.keys())[list(nums.values()).index(nums[str(v)])]
+        print(count)
 
         i += 1
-        break
+
+    return count
 
 # Input: n = 4
 # Output: "1211"
-countAndSay(4)
-# print(countAndSay(4))
+# countAndSay(4)
+print(countAndSay(4))

98360480156bfef0695b0dff25516c371c2f6bf1
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-15 11:47:37 -0600

    refactored code to make more readable with base strucutre

--- easy/strings/countsay.py
+++ easy/strings/countsay.py
@@ -1,10 +1,16 @@
+############------------ IMPORTS ------------############
 from collections import Counter
+from lib2to3.pgen2 import driver
 
+
+############------------ GLOBAL VARIABLE(S) ------------############
 nums = {
     '1': 'one','2': 'two','3': 'three','4': 'four','5': 'five',
     '6': 'six','7': 'seven','8': 'eight','9': 'nine','0': 'ten',
 }
 
+
+############------------ FUNCTION(S) ------------############
 def countAndSay(n: int) -> str:    
     i = 1
     count = str(i)
@@ -27,7 +33,10 @@ def countAndSay(n: int) -> str:
 
     return count
 
-# Input: n = 4
-# Output: "1211"
-# countAndSay(4)
-print(countAndSay(4))
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    # Input: n = 4
+    # Output: "1211"
+    # countAndSay(4)
+    print(countAndSay(4))

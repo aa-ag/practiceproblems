8329ccf99988431736a6cb2450a936240df2daca
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-15 11:08:48 -0600

    created loop with range of 1 to n+1 and corrected nums dictionary to have only strings as result is expected to be a string

--- easy/strings/countsay.py
+++ easy/strings/countsay.py
@@ -1,13 +1,16 @@
 nums = {
-    1: 'one',2: 'two',3: 'three',4: 'four',5: 'five',
-    6: 'six',7: 'seven',8: 'eight',9: 'nine',0: 'ten',
+    '1': 'one','2': 'two','3': 'three','4': 'four','5': 'five',
+    '6': 'six','7': 'seven','8': 'eight','9': 'nine','0': 'ten',
 }
 
 def countAndSay(n: int) -> str:
     a_value = nums[n]
     a_key = list(nums.keys())[list(nums.values()).index(nums[n])]
-    return a_value, a_key
+    
+    for i in range(1, n + 1):
+        print(i)
 
 # Input: n = 4
 # Output: "1211"
-print(countAndSay(4))
+countAndSay(4)
+# print(countAndSay(4))

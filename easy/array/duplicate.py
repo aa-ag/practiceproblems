008ad75d32279cd4d4bcc7a4c93156834ce725bf
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-21 16:11:27 -0600

    added own version of a naive solution

--- easy/array/duplicate.py
+++ easy/array/duplicate.py
@@ -1,11 +1,20 @@
 from typing import List
 
 def containsDuplicate(nums: List[int]) -> bool:
-    pass
+    uniques = list(set(nums))
+
+    for unique in uniques:
+        if nums.count(unique) > 1:
+            return True
+    
+    return False
 
 
 # True
-containsDuplicate([1,2,3,1])
+print(containsDuplicate([1,2,3,1]))
 
 # False
-containsDuplicate([1,2,3,4])
+print(containsDuplicate([1,2,3,4]))
+
+# True
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))

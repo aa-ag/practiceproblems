e7a52028ac9962d57aabc5e21ddcf156f2558d1e
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-24 19:09:09 -0600

    add intersect's base code from leetcode and two test cases

---
+++ easy/array/intersection.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
+    return nums1, nums2
+
+
+# Output: [2,2]
+print(intersect([1,2,2,1], [2,2]))
+
+# Output: [4,9]
+print(intersect([4,9,5], [9,4,9,8,4]))

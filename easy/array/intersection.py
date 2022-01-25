cba51cd225f1c15ea9eb379d7f7f4cf7ea3fc6ad
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-24 19:38:28 -0600

    Add elif to use different logic for lists that have the same length

--- easy/array/intersection.py
+++ easy/array/intersection.py
@@ -10,6 +10,8 @@ def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
                 answer.append(i)
                 nums2.remove(i)
         return answer
+    elif len(nums1) == len(nums2):
+        return [i for i in nums1 if i in nums2]
     else:
         for i in nums2:
             if i in nums1:
@@ -24,5 +26,8 @@ print(intersect([1,2,2,1], [2,2]))
 # Output: [4,9]
 print(intersect([4,9,5], [9,4,9,8,4]))
 
-# Output
+# Output [2]
 print(intersect([1,2,2,1], [2]))
+
+# Output
+print(intersect([2,1], [1,2]))

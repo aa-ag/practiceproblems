769136a9d88de29275b63169f842a2fe22dbad92
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-24 19:16:29 -0600

    add version that checks which list is longer

--- easy/array/intersection.py
+++ easy/array/intersection.py
@@ -2,8 +2,10 @@ from typing import List
 
 
 def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
-    return [i for i in nums1 if i in nums2]
-
+    if len(nums1) > len(nums2):
+        return [i for i in nums1 if i in nums2]
+    else:
+        return [i for i in nums2 if i in nums1]
 
 # Output: [2,2]
 print(intersect([1,2,2,1], [2,2]))

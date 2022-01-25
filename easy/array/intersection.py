9c23d7dbe4bd74f67693a5641e3cc514131da4c0
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-24 19:35:42 -0600

    Add new logic to check which list is longer and remove items as they are accounted for"

--- easy/array/intersection.py
+++ easy/array/intersection.py
@@ -1,11 +1,22 @@
 from typing import List
 
 
-def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:    
+    answer = list()
+
     if len(nums1) > len(nums2):
-        return [i for i in nums1 if i in nums2]
+        for i in nums1:
+            if i in nums2:
+                answer.append(i)
+                nums2.remove(i)
+        return answer
     else:
-        return [i for i in nums2 if i in nums1]
+        for i in nums2:
+            if i in nums1:
+                answer.append(i)
+                nums2.remove(i)
+                nums1.remove(i)
+        return answer
 
 # Output: [2,2]
 print(intersect([1,2,2,1], [2,2]))

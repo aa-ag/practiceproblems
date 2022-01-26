1c5357345db6f579dcc040ed328292ed428f48a3
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-25 19:56:32 -0600

    Add second test case from Leetcode, still trying to fully understand if new array should also be returned"

--- easy/array/dedupe.py
+++ easy/array/dedupe.py
@@ -8,3 +8,8 @@ def removeDuplicates(nums: List[int]) -> int:
 # nums = [1,1,2]
 # output: 2, nums = [1,2,_]
 print(removeDuplicates([1,1,2]))
+
+
+# nums = [0,0,1,1,1,2,2,3,3,4]
+# output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))

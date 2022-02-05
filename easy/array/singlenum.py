ffb23012a7c9478a39788ecd391a2341b2acdb72
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-05 05:49:04 -0600

    Add print statements instead of function calls to be able to return results within method

--- easy/array/singlenum.py
+++ easy/array/singlenum.py
@@ -1,4 +1,9 @@
 from typing import List
 
 def singleNumber(nums: List[int]) -> int:
-    pass
+    return nums
+
+
+print(singleNumber([2,2,1]))
+print(singleNumber([4,1,2,1,2]))
+print(singleNumber([1]))

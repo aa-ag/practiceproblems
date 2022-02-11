5ae2c05a6d188bc3e477d25308b6bb3748aefbea
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-10 19:47:09 -0600

    Add solution from LeetCode with significant better memory usage

--- easy/array/plusone.py
+++ easy/array/plusone.py
@@ -1,7 +1,17 @@
 from typing import List
 
 def plusOne(digits: List[int]) -> List[int]:
-    return [i for i in str(int(''.join(str(digit) for digit in digits)) + 1)]
+    carry  = 1    
+    for i in range(len(digits)-1, -1, -1):
+        if carry + digits[i] >= 10:
+            digits[i] = digits[i] + carry  - 10
+            carry = 1
+        else:
+            digits[i] = digits[i] + 1
+            return digits
+    if carry:
+        digits.insert(0, carry)
+    return digits
 
 
 print(plusOne([1,2,3]))

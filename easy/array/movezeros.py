0754fc752daec67c092561b7f8257cf06f7ee63c
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-11 20:16:54 -0600

    Added loop to pop and append to try out

--- easy/array/movezeros.py
+++ easy/array/movezeros.py
@@ -4,9 +4,15 @@ def moveZeroes(nums: List[int]) -> None:
     '''
      do not return anything: modify in place
     '''
-    pass
+
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            nums.pop(i)
+            nums.append(0)
 
 
-moveZeroes()
-moveZeroes()
-moveZeroes()
+
+moveZeroes([0,1,0,3,12])
+# expected output: [1,3,12,0,0]
+moveZeroes([0])
+# expected output: [0]ß

89f807f1105a2083828c677c35b178037c0b189c
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-12 18:17:43 -0600

    Add first attempt: WIP

--- easy/array/twosum.py
+++ easy/array/twosum.py
@@ -1,7 +1,16 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    return nums
+    temp = 0
+    answer = list()
+
+    while temp < target:
+        for n, num in enumerate(nums):
+            temp += num
+            print(temp)
+            answer.append(n)
+    
+    return answer
 
 
 # Input: nums = [2,7,11,15], target = 9
@@ -10,8 +19,8 @@ print(twoSum([2,7,11,15], 9))
 
 # Input: nums = [3,2,4], target = 6
 # Output: [1,2]
-print(twoSum([3,2,4], 6))
+# print(twoSum([3,2,4], 6))
 
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
-print(twoSum([3,3], 6))
+# print(twoSum([3,3], 6))

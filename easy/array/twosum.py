74713feeacbb626e559f3a567db89abb35424e6b
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-12 18:10:26 -0600

    Add three test cases with Inputs provided and expected Output

--- easy/array/twosum.py
+++ easy/array/twosum.py
@@ -2,3 +2,15 @@ from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
     pass
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+twoSum([2,7,11,15])
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+twoSum([3,2,4])
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+twoSum([3,3])

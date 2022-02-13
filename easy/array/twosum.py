c9ad151dbeb869eadcf806c66ad2f233d62378fb
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-12 18:11:52 -0600

    Add targets as parameters to each test/function call

--- easy/array/twosum.py
+++ easy/array/twosum.py
@@ -1,16 +1,17 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    pass
+    return nums
+
 
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
-twoSum([2,7,11,15])
+print(twoSum([2,7,11,15], 9))
 
 # Input: nums = [3,2,4], target = 6
 # Output: [1,2]
-twoSum([3,2,4])
+print(twoSum([3,2,4], 6))
 
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
-twoSum([3,3])
+print(twoSum([3,3], 6))

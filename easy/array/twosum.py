ee7fe2cff821163e9b0cb852be7990a78089081b
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-12 19:58:56 -0600

    Add refactor without the need for an additional answer-list, instead once sum of two is found, returns list with two indexes and stops

--- easy/array/twosum.py
+++ easy/array/twosum.py
@@ -1,13 +1,10 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    answer = list()
-
     for i in range(len(nums)):
         for j in range(1, len(nums)):
             if nums[i] + nums[j] == target:
-                answer.append((i, j))
-    return answer
+                return [i, j]
 
 
 # Input: nums = [2,7,11,15], target = 9

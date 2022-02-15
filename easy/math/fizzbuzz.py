10caa73e524f9da86c7bd2219a4032bc337ededa
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-14 22:17:16 -0600

    Add Buzz append if divisible by 5 and not by 3 AND refactored first if/check to if divisible by 3 and not 5

--- easy/math/fizzbuzz.py
+++ easy/math/fizzbuzz.py
@@ -6,8 +6,10 @@ def fizzBuzz(n: int) -> List[str]:
     answer = list()
 
     for i in range(1, n + 1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 != 0:
             answer.append("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            answer.append("Buzz")
         else:
             answer.append(i)
     return answer

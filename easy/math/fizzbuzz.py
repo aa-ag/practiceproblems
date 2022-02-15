78c5b9f4f11c6431aa33104c6223bc52428927b8
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-14 22:13:28 -0600

    Add loop to generate list of intengers within range

--- easy/math/fizzbuzz.py
+++ easy/math/fizzbuzz.py
@@ -1,6 +1,11 @@
+from tracemalloc import start
 from typing import List
 
 
 def fizzBuzz(n: int) -> List[str]:
-    pass
+    return [i for i in range(1, n + 1)]
         
+
+print(fizzBuzz(3))
+print(fizzBuzz(5))
+print(fizzBuzz(15))

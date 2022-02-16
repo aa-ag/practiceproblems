23497172b134e0fd91cf472db7c0e636587a1c38
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-16 17:45:59 -0600

    Add fastest possible way to find out O(1) for both space and time using the max integer that a power of 3 can hold: 3**9 (3^9)

--- easy/math/powerofthree.py
+++ easy/math/powerofthree.py
@@ -1,16 +1,23 @@
-
 def is_power_of_three(n: int) -> bool:
     '''
-     O(log3(n)) time complexity: where 3 is the number of divisions
-     O(1) space complexity: no additional space required
+     O(1) time complexity: only one operation takes place
+     O(1) space complexity: no additional memory is used
     '''
-    if n < 1:
-        return False
+    return n > 0 and 1162261467 % n == 0
 
-    while n % 3 == 0:
-        n /= 3
 
-    return n == 1
+# def is_power_of_three(n: int) -> bool:
+#     '''
+#      O(log3(n)) time complexity: where 3 is the number of divisions
+#      O(1) space complexity: no additional space required
+#     '''
+#     if n < 1:
+#         return False
+
+#     while n % 3 == 0:
+#         n /= 3
+
+#     return n == 1
 
 
 print(is_power_of_three(27))

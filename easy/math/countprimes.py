dcdb4a9ccc0f819813803b2158eb5f3a2499f40d
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-15 21:48:53 -0600

    Add version of count_primes that has a worst-case time complexity of O(n* log(logN)) as it traverses thru n numbers in the range, but only iterates thru multiples of each n

--- easy/math/countprimes.py
+++ easy/math/countprimes.py
@@ -2,16 +2,18 @@ from typing import List
 
 
 def count_primes(n: int) -> int:
+    dp = [True] * n
+    
     count = 0
-
-    for i in range(n):
-        if i <= 1:
-            continue
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
+    for i in range(2,n):
+        if dp[i]:
             count += 1
+        if not dp[i]: 
+            continue
+            
+        for j in range(i+i, n, i):
+            dp[j] = False
+        
     return count
 
 

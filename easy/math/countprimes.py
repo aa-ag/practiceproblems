672e9d7b8f88a894dd47c8dd62142ac26ee467a2
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-15 21:40:48 -0600

    Add version that works with small numbers

--- easy/math/countprimes.py
+++ easy/math/countprimes.py
@@ -2,4 +2,20 @@ from typing import List
 
 
 def count_primes(n: int) -> int:
-    pass
+    count = 0
+
+    for i in range(n):
+        if i <= 1:
+            continue
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            count += 1
+    return count
+
+
+print(count_primes(0))
+print(count_primes(1))
+print(count_primes(10))
+

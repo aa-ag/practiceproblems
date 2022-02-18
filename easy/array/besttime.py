ec58605d7fb0bd0d982557094f1b9bca1cc63c1f
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-17 20:15:57 -0600

    Add three test cases for max_profit from LeetCode

--- easy/array/besttime.py
+++ easy/array/besttime.py
@@ -2,3 +2,16 @@ from typing import List
 
 def max_profit(prices: List[int]) -> int:
     pass
+
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+print(max_profit([7,1,5,3,6,4]))
+
+# Input: prices = [1,2,3,4,5]
+# Output: 4
+print(max_profit([1,2,3,4,5]))
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+print(max_profit([7,6,4,3,1]))

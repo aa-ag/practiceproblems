3cd72b3838677454ebd4c8b8518dd7c4e0f1aec8
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-18 22:34:34 -0600

    Add comments with algo to find compound combinations, and refactored logic to slice string with index

--- easy/math/romannumberals.py
+++ easy/math/romannumberals.py
@@ -1,20 +1,23 @@
-from code import interact
-
-
+### GLOBAL
 d = {
     "I": 1, "V": 5, "X": 10, "L": 50,
     "C": 100, "D": 500, "M": 1000
 }
 
+### FUNCTION(S)
 def roman_to_int(s: str) -> int:
     integer = 0
 
-    for character in s:
-        integer += d[character]
+    for i in range(len(s)):
+        # check if next character is larger,
+        # if it is, must be compound combo
+        # else, += to integer
+        integer += d[s[i]]
 
     return integer
 
 
+### DRIVER
 # Input: s = "III"
 # Output: 3
 print(roman_to_int("III"))

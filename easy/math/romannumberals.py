8c50d85feba1e94bae409f20af648de722cc368d
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-18 22:20:55 -0600

    Add three test cases to roman_to_int

--- easy/math/romannumberals.py
+++ easy/math/romannumberals.py
@@ -1,16 +1,16 @@
 
 def roman_to_int(s: str) -> int:
-    pass
+    return s
 
 
 # Input: s = "III"
 # Output: 3
-print(roman_to_int())
+print(roman_to_int("III"))
 
 # Input: s = "LVIII"
 # Output: 58
-print(roman_to_int())
+print(roman_to_int("LVIII"))
 
 # Input: s = "MCMXCIV"
 # Output: 1994
-print(roman_to_int())
+print(roman_to_int("MCMXCIV"))

7f7d50836bc8cb768443e85950712a2d544b29d3
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-18 22:19:39 -0600

    Add roman_to_int refactored to exist outside Solution class from LeetCode

---
+++ easy/math/romannumberals.py
@@ -0,0 +1,16 @@
+
+def roman_to_int(s: str) -> int:
+    pass
+
+
+# Input: s = "III"
+# Output: 3
+print(roman_to_int())
+
+# Input: s = "LVIII"
+# Output: 58
+print(roman_to_int())
+
+# Input: s = "MCMXCIV"
+# Output: 1994
+print(roman_to_int())

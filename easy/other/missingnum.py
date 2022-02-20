75541de14e7a8b0afbeb64758c299b67e5b24366
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-19 20:55:20 -0600

    Add inputs into test scenarios and return a sorted version of nums

--- easy/other/missingnum.py
+++ easy/other/missingnum.py
@@ -2,17 +2,17 @@ from typing import List
 
 
 def missing_number(nums: List[int]) -> int:
-    pass
+    return sorted(nums)
 
 
 # Input: nums = [3,0,1]
 # Output: 2
-print(missing_number)
+print(missing_number([3,0,1]))
 
 # Input: nums = [0,1]
 # Output: 2
-print(missing_number)
+print(missing_number([0,1]))
 
 # Input: nums = [9,6,4,2,3,5,7,0,1]
 # Output: 8
-print(missing_number)
+print(missing_number([9,6,4,2,3,5,7,0,1]))

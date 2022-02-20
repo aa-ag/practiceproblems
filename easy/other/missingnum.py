5958699570310128117ff8bcf6cae43d922ab91e
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-19 20:53:14 -0600

    Add three case scenarios to test mising_number()

--- easy/other/missingnum.py
+++ easy/other/missingnum.py
@@ -4,3 +4,15 @@ from typing import List
 def missing_number(nums: List[int]) -> int:
     pass
 
+
+# Input: nums = [3,0,1]
+# Output: 2
+print(missing_number)
+
+# Input: nums = [0,1]
+# Output: 2
+print(missing_number)
+
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
+print(missing_number)

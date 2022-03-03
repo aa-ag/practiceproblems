7e1955a53e15227d5c5fcb7cdb661d3e5348276a
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-02 18:43:35 -0600

    Add steps version passing all tests

--- easy/dynamic/stairs.py
+++ easy/dynamic/stairs.py
@@ -1,8 +1,21 @@
 
+from tracemalloc import start
+
+
 def climb_stairs(n: int) -> int:
-    pass
+    if n == 1 or n == 2:
+        return n
+    else:
+        first_step = 1
+        second_step = 2
+        for step in range(3, n+1):
+            x = first_step + second_step
+            first_step = second_step
+            second_step = x
+        return x
 
 
-print(climb_stairs())
 
-print(climb_stairs())
+print(climb_stairs(2))
+
+print(climb_stairs(3))

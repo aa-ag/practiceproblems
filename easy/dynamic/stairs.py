131e5c446700038e8fe5fbba9764f3890d7b8f31
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-02 18:45:40 -0600

    Add version from leetcode which uses a bit more memory but is a lot more performant

--- easy/dynamic/stairs.py
+++ easy/dynamic/stairs.py
@@ -1,18 +1,27 @@
 
-from tracemalloc import start
-
-
 def climb_stairs(n: int) -> int:
-    if n == 1 or n == 2:
-        return n
-    else:
-        first_step = 1
-        second_step = 2
-        for step in range(3, n+1):
-            x = first_step + second_step
-            first_step = second_step
-            second_step = x
-        return x
+    stairs = [0] * (n + 2)
+    stairs[1] = 1
+    stairs[2] = 2
+
+    
+    for i in range(3, len(stairs)):
+        stairs[i] = stairs[i - 1] + stairs[i - 2]
+    
+    return stairs[n]
+
+
+# def climb_stairs(n: int) -> int:
+#     if n == 1 or n == 2:
+#         return n
+#     else:
+#         first_step = 1
+#         second_step = 2
+#         for step in range(3, n+1):
+#             x = first_step + second_step
+#             first_step = second_step
+#             second_step = x
+#         return x
 
 
 

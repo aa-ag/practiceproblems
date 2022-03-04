92bb4a654b2cf06b7681a727e03d6511bb7c6e25
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-03 20:05:00 -0600

    Failed one of the test cases; rethinking for now

--- easy/other/validparenthesis.py
+++ easy/other/validparenthesis.py
@@ -2,21 +2,12 @@
 
 def is_valid(s: str) -> bool:
     '''
-     create an empty list to use as a stack,
-     add to stack when opening parenthesis is found,
-     pop from the stack when closing parenthesis is found
+     need to rethink this one
     '''
-    stack = list()
-
-    for i in s:
-        if i in ['(', '[', '\{']:
-            stack.append(i)
-        elif i in [')', ']', '\}']:
-            stack.pop()
-
-    return not len(stack)
+    pass
 
 
+print(is_valid("()"))
 
 print(is_valid("()[]\{\}"))
 

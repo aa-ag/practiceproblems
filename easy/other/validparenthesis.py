35c81cb45a10bb8b4b19b74e3ffce0d210425a94
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-04 20:16:48 -0600

    Adde version from another student from Leetcode using a stack

--- easy/other/validparenthesis.py
+++ easy/other/validparenthesis.py
@@ -1,14 +1,30 @@
-
-
 def is_valid(s: str) -> bool:
-    '''
-     need to rethink this one
-    '''
-    while '()' in s or '\{\}' in s or '[]' in s:
-        s = s.replace('()', '').replace('\{\}', '').replace('[]', '')
-    if s == '':
-        return True
-    return False
+    stack = list()
+    ch_opp = {
+        ')':'(', 
+        '}':'{', 
+        ']':'['
+    }
+    
+    for ch in s:
+        if ch in ch_opp:
+            if not stack or stack[-1] != ch_opp[ch]:
+                return False
+            stack.pop()
+        else:
+            stack.append(ch)
+    return not stack
+
+
+# def is_valid(s: str) -> bool:
+#     '''
+#      need to rethink this one
+#     '''
+#     while '()' in s or '\{\}' in s or '[]' in s:
+#         s = s.replace('\{\}', '').replace('()', '').replace('[]', '')
+#     if s == '':
+#         return True
+#     return False
 
 # True
 print(is_valid("()"))
@@ -19,4 +35,4 @@ print(is_valid("()[]\{\}"))
 # False
 print(is_valid("(]"))
 
-# print(is_valid("{ { ( { } ) } }"))
+print(is_valid("{ { ( { } ) } }"))

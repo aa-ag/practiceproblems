c8777004e2f6783529eaa0f3526c933c9540b67a
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-04 20:05:05 -0600

    Add replace logic to check validity

--- easy/other/validparenthesis.py
+++ easy/other/validparenthesis.py
@@ -4,13 +4,19 @@ def is_valid(s: str) -> bool:
     '''
      need to rethink this one
     '''
-    pass
+    while '()' in s or '\{\}' in s or '[]' in s:
+        s = s.replace('()', '').replace('\{\}', '').replace('[]', '')
+    if s == '':
+        return True
+    return False
 
-
+# True
 print(is_valid("()"))
 
+# True
 print(is_valid("()[]\{\}"))
 
+# False
 print(is_valid("(]"))
 
-print(is_valid("{ { ( { } ) } }"))
+# print(is_valid("{ { ( { } ) } }"))

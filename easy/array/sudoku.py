88e7be5b03e20c5e05e0066b9759873dd0412a2d
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-06 19:24:15 -0600

    Add logic to create 3x3 grids found on Reddit: brute force approach

--- easy/array/sudoku.py
+++ easy/array/sudoku.py
@@ -1,13 +1,27 @@
 from typing import List
-import numpy as np
+# import numpy as np
+
+# TODO: check this out: 
+# https://www.youtube.com/watch?v=ClvYxCvzpEQ
+
 
 def is_valid(board: List[List[str]]) -> bool:
-    for row in board:
-        nums = [int(i) for i in row if i != '.']
-        for n in nums:
-            if n > 9 or n < 0 or nums.count(n) > 1:
-                return False
-        print(row)
+    # for row in board:
+    #     nums = [int(i) for i in row if i != '.']
+    #     for n in nums:
+    #         if n > 9 or n < 0 or nums.count(n) > 1:
+    #             return False
+    #     print(row)
+    three_by_three = list()
+
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            block = board[i][j:j+3] + \
+                board[i+1][j:j+3] + \
+                    board[i+2][j:j+3]
+            three_by_three.append(block)
+        
+    print(three_by_three)
 
 
 board = [

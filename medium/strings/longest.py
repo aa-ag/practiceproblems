b7bf3dab417aeda491792581c7ca79f55a7b98a6
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-07 17:58:06 -0600

    Add three test cases

--- medium/strings/longest.py
+++ medium/strings/longest.py
@@ -4,6 +4,16 @@ def lengthOfLongestSubstring(s: str) -> int:
     pass
 
 
+# Input: s = "abcabcbb"
+# Output: 3
 print(lengthOfLongestSubstring())
 
+
+# Input: s = "bbbbb"
+# Output: 1
+print(lengthOfLongestSubstring())
+
+
+# Input: s = "pwwkew"
+# Output: 3
 print(lengthOfLongestSubstring())

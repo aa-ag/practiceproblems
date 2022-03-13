b49bf431faefa2df59e6180cb2f6e3d95810d6bf
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-13 10:45:04 -0500

    a

---
+++ medium/other/majority.py
@@ -0,0 +1,15 @@
+############------------ IMPORTS ------------############
+from typing import List
+from unicodedata import name
+
+
+############------------ FUNCTION(S) ------------############
+def majority_element(nums: List[int]) -> int:
+    pass
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    print(majority_element())
+
+    

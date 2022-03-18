3c15e130c9e15421676e4a4168f3aa6638e4f8c9
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-17 20:04:52 -0500

    Add two test cases

--- medium/other/sum.py
+++ medium/other/sum.py
@@ -5,4 +5,17 @@
 def get_sum(a: int, b: int) -> int:
     pass
 
+
 ############------------ DRIVER CODE ------------############
+'''
+Input: a = 1, b = 2
+Output: 3
+'''
+print(get_sum())
+
+'''
+Input: a = 2, b = 3
+Output: 5
+'''
+print(get_sum())
+

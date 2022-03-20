56e8da630a14950b4cd539704943dc9ed0ef89ef
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-19 21:24:32 -0500

    Add three test cases

--- medium/math/pow.py
+++ medium/math/pow.py
@@ -4,3 +4,16 @@ def my_pow(x: float, n: int) -> float:
      calculates x to the power of n
     '''
     pass
+
+
+# Input: x = 2.00000, n = 10
+# Output: 1024.00000
+print(my_pow(2.00000, 10))
+
+# Input: x = 2.10000, n = 3
+# Output: 9.26100
+print(my_pow(2.10000, 3))
+
+# Input: x = 2.00000, n = -2
+# Output: 0.25000
+print(my_pow(2.00000, 2))

f9922b032a43e573e77f519d70d0fd56230a3bd6
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-22 21:19:53 -0500

    refactor is_happy to exist outside of Solution class

---
+++ medium/math/happy.py
@@ -0,0 +1,9 @@
+############------------ FUNCTION(S) ------------############
+def is_happy(n: int) -> bool:
+    pass
+
+
+############------------ DRIVER CODE ------------############
+print(is_happy())
+
+print(is_happy())

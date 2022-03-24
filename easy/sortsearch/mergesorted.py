d4e1a5f81ea2eda496c7a71bb26d85595e2fcaae
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-23 21:50:00 -0500

    Add two more test cases and their explanations

--- easy/sortsearch/mergesorted.py
+++ easy/sortsearch/mergesorted.py
@@ -11,3 +11,11 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 # Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 # Output: [1,2,2,3,5,6]
 print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
+
+# Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# Output: [1]
+print(merge([1], 1, [], 0))
+
+# Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+# Output: [1]
+print(merge([0], 0, [1], 1))

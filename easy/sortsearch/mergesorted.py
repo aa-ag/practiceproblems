cf66f3dec71b081d2b2f700743b980e646b28ba1
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-23 21:46:26 -0500

    Add merge to exist outside Solutions class

---
+++ easy/sortsearch/mergesorted.py
@@ -0,0 +1,11 @@
+############------------ IMPORTS ------------############
+from typing import List
+
+
+############------------ FUNCTION(S) ------------############
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    pass
+
+
+############------------ DRIVER CODE ------------############
+print(merge())

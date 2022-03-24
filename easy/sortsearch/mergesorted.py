529cb16353d5cfc66fd6c4b2d0fd96310fb5cae6
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-23 21:48:15 -0500

    Add a test case

--- easy/sortsearch/mergesorted.py
+++ easy/sortsearch/mergesorted.py
@@ -4,8 +4,10 @@ from typing import List
 
 ############------------ FUNCTION(S) ------------############
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-    pass
+    return nums1, m, nums2, n
 
 
 ############------------ DRIVER CODE ------------############
-print(merge())
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))

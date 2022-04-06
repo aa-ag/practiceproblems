8662d2dc5f1e6ba45e1b6fc0af3b5ed287c42f0d
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-05 22:56:38 -0500

    Add print statement for test case and range from 1 to n to get started

--- easy/sortsearch/firstbad.py
+++ easy/sortsearch/firstbad.py
@@ -1,9 +1,9 @@
 ############------------ FUNCTION(S) ------------############
 def first_bad_version(n: int) -> int:
-    pass
+    return [i for i in range(1, n)]
 
 
 ############------------ DRIVER CODE ------------############
 # Input: n = 5, bad = 4
 # Output: 4
-first_bad_version(5)
+print(first_bad_version(5))

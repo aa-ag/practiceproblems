10430b60e29797a434569a2227b8fd00bc00f6fc
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-05 23:06:17 -0500

    Slice first bad from r list

--- easy/sortsearch/firstbad.py
+++ easy/sortsearch/firstbad.py
@@ -1,9 +1,9 @@
 ############------------ FUNCTION(S) ------------############
 def first_bad_version(n: int) -> int:
     r = [i for i in range(n-1, -1, -1)]
-    return r
+    return r[0]
 
-
+    
 ############------------ DRIVER CODE ------------############
 # Input: n = 5, bad = 4
 # Output: 4

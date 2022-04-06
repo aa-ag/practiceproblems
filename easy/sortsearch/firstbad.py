b8944699679454086438017c9604459c81cfa766
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-05 23:09:51 -0500

    Add binary search tree

--- easy/sortsearch/firstbad.py
+++ easy/sortsearch/firstbad.py
@@ -1,9 +1,17 @@
 ############------------ FUNCTION(S) ------------############
 def first_bad_version(n: int) -> int:
-    r = [i for i in range(n-1, -1, -1)]
-    return r[0]
+    left, right = 1, n
 
-    
+    while left <= right:
+        mid = (left+right) // 2
+        if isBadVersion(mid):
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left
+
+
 ############------------ DRIVER CODE ------------############
 # Input: n = 5, bad = 4
 # Output: 4

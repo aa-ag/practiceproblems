83720eeb7514312d2a26ce2edf1e7cf71c617e58
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-11 20:21:09 -0500

    Add two test cases

--- easy/dynamic/rob.py
+++ easy/dynamic/rob.py
@@ -2,4 +2,12 @@ from typing import List
 
 def rob(nums: List[int]) -> int:
     pass
-        
+
+
+# Input: nums = [1,2,3,1]
+# Output: 4
+print(rob([1,2,3,1]))
+
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+print(rob([2,7,9,3,1]))

75167de8565bf7e3b01d83817ad726ec8103823d
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-12 19:54:52 -0500

    Add solution from Solutions section which is a bit slower, tho considerably more space efficient

--- easy/dynamic/rob.py
+++ easy/dynamic/rob.py
@@ -1,15 +1,10 @@
 from typing import List
 
 def rob(nums: List[int]) -> int:
-    rob_prev = nums[0]
-    not_rob_prev = 0
-
-    for num in nums[1:]:
-        rob_current = num + not_rob_prev
-        not_rob_current = max(rob_prev, not_rob_prev)
-        rob_prev, not_rob_prev = rob_current, not_rob_current
-    
-    return max(rob_prev, not_rob_prev)
+    last, now = 0, 0
+    for n in nums:
+        last, now = now, max(last + n, now)
+    return now
 
 
 # Input: nums = [1,2,3,1]

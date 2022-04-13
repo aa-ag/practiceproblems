b3d7cc4265a1b983f9b89a8273c229e25c03e593
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-12 20:25:23 -0500

    Add logic to print out combinations of subsets of a list with itertools combinations import

--- medium/backtracking/subset.py
+++ medium/backtracking/subset.py
@@ -1,13 +1,16 @@
 from typing import List
+import itertools
 
 def subsets(nums: List[int]) -> List[List[int]]:
-    pass
+    answer = list()
+    for i in range(len(nums)):
+        print([list(j) for j in itertools.combinations(nums, i)])
 
 
 # Input: nums = [1,2,3]
 # Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-print(subsets())
+print(subsets([1,2,3]))
 
 # Input: nums = [0]
 # Output: [[],[0]]
-print(subsets())
+# print(subsets())

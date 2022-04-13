6151bc2fbd3488c74f3658be231fdf7bdc4140f1
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-12 20:04:37 -0500

    Add two test cases

--- medium/backtracking/subset.py
+++ medium/backtracking/subset.py
@@ -2,3 +2,12 @@ from typing import List
 
 def subsets(nums: List[int]) -> List[List[int]]:
     pass
+
+
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+print(subsets())
+
+# Input: nums = [0]
+# Output: [[],[0]]
+print(subsets())

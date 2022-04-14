19a539bb1520024e36640db13adae8c4719e5e72
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-13 19:29:46 -0500

    Add logic to check if all nums in nums are less than 0

--- hard/arrays_and_strings/firstmissing.py
+++ hard/arrays_and_strings/firstmissing.py
@@ -1,6 +1,8 @@
 from typing import List
 
 def first_missing_positive(nums: List[int]) -> int:
+    if all(num < 0 for num in nums):
+        return 1
     nums.sort()
     for i in range(1, max(nums) + 2):
         if i not in nums:
@@ -16,5 +18,10 @@ print(first_missing_positive([1,2,0]))
 print(first_missing_positive([3,4,-1,1]))
 
 # Input: nums = [7,8,9,11,12]
+# Output: 1
+print(first_missing_positive([7,8,9,11,12]))
+
+
+# Input: nums = [-5]
 # Output: 1
 print(first_missing_positive([7,8,9,11,12]))

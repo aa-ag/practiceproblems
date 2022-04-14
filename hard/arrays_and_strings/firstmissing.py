a7c11d2148fec9315b401b7d4417423f4392b72b
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-13 19:20:34 -0500

    Add three test cases from LeetCode

--- hard/arrays_and_strings/firstmissing.py
+++ hard/arrays_and_strings/firstmissing.py
@@ -4,7 +4,14 @@ def first_missing_positive(nums: List[int]) -> int:
     pass
 
 
-print(first_missing_positive())
+# Input: nums = [1,2,0]
+# Output: 3
+print(first_missing_positive([1,2,0]))
 
+# Input: nums = [3,4,-1,1]
+# Output: 2
+print(first_missing_positive([3,4,-1,1]))
 
-print(first_missing_positive())
+# Input: nums = [7,8,9,11,12]
+# Output: 1
+print(first_missing_positive([7,8,9,11,12]))

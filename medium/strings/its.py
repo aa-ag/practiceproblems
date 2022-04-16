7ce3d6d7325a1bdf49b15d25c5251502d4292ff7
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-15 21:56:59 -0500

    Add two test cases from Leetcode

--- medium/strings/its.py
+++ medium/strings/its.py
@@ -3,3 +3,12 @@ from typing import List
 
 def increasing_triplet(nums: List[int]) -> bool:
     pass
+
+
+# Input: nums = [1,2,3,4,5]
+# Output: true
+print(increasing_triplet())
+
+# Input: nums = [5,4,3,2,1]
+# Output: false
+print(increasing_triplet())

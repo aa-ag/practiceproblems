3946aa22374327c889440903cc51adb92e77f150
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-15 21:58:13 -0500

    Pass test data to each printing of function and add third test case from leetcode

--- medium/strings/its.py
+++ medium/strings/its.py
@@ -7,8 +7,12 @@ def increasing_triplet(nums: List[int]) -> bool:
 
 # Input: nums = [1,2,3,4,5]
 # Output: true
-print(increasing_triplet())
+print(increasing_triplet([1,2,3,4,5]))
 
 # Input: nums = [5,4,3,2,1]
 # Output: false
-print(increasing_triplet())
+print(increasing_triplet([5,4,3,2,1]))
+
+# Input: nums = [2,1,5,0,4,6]
+# Output: true
+print(increasing_triplet([2,1,5,0,4,6]))

88f421d8dffb561e912d259421951ad1891d3ae7
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-17 14:26:27 -0500

    Add new version which just checks if key exists else it creates it, else it populates just the value using that key in an "answer" dictionary

--- medium/strings/groupanagrams.py
+++ medium/strings/groupanagrams.py
@@ -1,29 +1,15 @@
 from typing import List
 
 def group_anagrams(strs: List[str]) -> List[List[str]]:
-    if len(strs) == 1:
-        return [strs]
-
-    answer = list()
-    anagrams = set()
-
-    for str in strs:
-        anagrams.add(''.join(sorted(str)))
-
     d = dict()
 
-    for anagram in list(anagrams):
-        d[anagram] = list()
-
-    for k, v in d.items():
-        for i in strs:
-            if ''.join(sorted(i)) == k:
-                d[k].append(i)
-            
-    for value in d.values():
-        answer.append(value)
-    
-    return answer
+    for str in strs:
+        k = ''.join(sorted(str))
+        if k not in d:
+            d[k] = [str]
+        else:
+            d[k].append(str)
+    return d.values()
 
 # Input: strs = ["eat","tea","tan","ate","nat","bat"]
 # Output: [["bat"],["nat","tan"],["ate","eat","tea"]]

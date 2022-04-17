6e4053bff35579d0863d8f77b539d32974b44f38
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-16 21:46:53 -0500

    Add solution using math import

--- medium/strings/its.py
+++ medium/strings/its.py
@@ -1,24 +1,21 @@
 ### Increasing Triple Subsequence
 from typing import List
+import math
 
 def increasing_triplet(nums: List[int]) -> bool:
     '''
       return true if there exists a triple of indices (i, j, k) 
       such that i < j < k and nums[i] < nums[j] < nums[k]
     '''
-    min_firsts = float('inf')
-    min_seconds = float('inf')
-    
-    for num in nums:
-        if num > min_seconds:
-            return True
-        elif num > min_firsts:
-            min_seconds = min(min_seconds, num)
+    first, second = math.inf, math.inf    
+    for i in nums:
+        if i <= first:
+            first = i
+        elif i <= second:
+            second = i
         else:
-            min_firsts = min(min_firsts, num)
-    
+            return True
     return False
-
 
 # Input: nums = [1,2,3,4,5]
 # Output: true

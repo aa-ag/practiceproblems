066ddc63808ad054ab208feb086d8995bdd40403
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-17 14:01:30 -0500

    Add provisional logic to check sorted strings within strs

--- medium/strings/groupanagrams.py
+++ medium/strings/groupanagrams.py
@@ -1,13 +1,16 @@
 from typing import List
 
 def group_anagrams(strs: List[str]) -> List[List[str]]:
-    pass
+    anagrams = list()
+
+    for i in range(len(strs)):
+        print(sorted(strs[i]))
 
 
 # Input: strs = ["eat","tea","tan","ate","nat","bat"]
 # Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-print(group_anagrams())
+print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
 
 # Input: strs = [""]
 # Output: [[""]]
-print(group_anagrams())
+print(group_anagrams([""]))

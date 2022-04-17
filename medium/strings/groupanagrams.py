9d12038563c227df5c59c0fdef8a14e575ab50ed
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-17 14:00:37 -0500

    Add two test cases

--- medium/strings/groupanagrams.py
+++ medium/strings/groupanagrams.py
@@ -2,3 +2,12 @@ from typing import List
 
 def group_anagrams(strs: List[str]) -> List[List[str]]:
     pass
+
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+print(group_anagrams())
+
+# Input: strs = [""]
+# Output: [[""]]
+print(group_anagrams())

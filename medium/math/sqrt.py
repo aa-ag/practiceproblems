e5d1b0b76a36bcaccaff7fb09e841c00acf10f6b
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-23 20:55:52 -0500

    Add solution from another student with better perfomance

--- medium/math/sqrt.py
+++ medium/math/sqrt.py
@@ -1,8 +1,22 @@
-import math
+# import math
+
+# def sqrt(x: int) -> int:
+    # return int(math.sqrt(x))
 
 def sqrt(x: int) -> int:
-    return int(math.sqrt(x))
+    l = 0
+    h = x
 
+    while l <= h:
+        m = (l+h) // 2
+
+        if m*m == x:
+            return m
+        if m*m > x:
+            h = m - 1
+        else:
+            l = m + 1
+    return h
 
 # Input: x = 4
 # Output: 2

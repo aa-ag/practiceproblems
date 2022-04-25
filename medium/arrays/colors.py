f74fca2a07f0f14d06e56a28c4b6cab41c112a64
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-24 21:01:18 -0500

    Add logic from stackoverflow on nested for loops

--- medium/arrays/colors.py
+++ medium/arrays/colors.py
@@ -4,7 +4,12 @@ def sort_colors(nums: List[int]) -> None:
     """
      Do not return anything, modify nums in-place instead.
     """
-    return nums
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] > nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+    
+    print(nums)
 
 
 # Input: nums = [2,0,2,1,1,0]

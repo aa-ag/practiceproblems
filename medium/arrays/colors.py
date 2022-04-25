053700556439cfc8442cbe2611e330bf77a6acfa
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-24 20:41:07 -0500

    Add two test cases

--- medium/arrays/colors.py
+++ medium/arrays/colors.py
@@ -4,4 +4,14 @@ def sort_colors(nums: List[int]) -> None:
     """
      Do not return anything, modify nums in-place instead.
     """
-    pass
+    return nums
+
+
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+print(sort_colors([2,0,2,1,1,0]))
+
+
+# Input: nums = [2,0,1]
+# Output: [0,1,2]
+print(sort_colors([2,0,1]))

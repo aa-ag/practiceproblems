ee923bb38e30b33310e88eaf00bec1ecf51fb556
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-24 21:12:15 -0500

    Add another version from another student where if an index is zero, it's moved to the left, and if it is 2 it is moved to the right, leaving all 1's in the middle

--- medium/arrays/colors.py
+++ medium/arrays/colors.py
@@ -4,7 +4,19 @@ def sort_colors(nums: List[int]) -> None:
     """
      Do not return anything, modify nums in-place instead.
     """
-    nums.sort()
+    left, right = 0, len(nums) - 1
+        
+    i = 0
+        
+    while i <= right:
+        if nums[i] == 0:
+            nums[i], nums[left] = nums[left], nums[i]
+            left += 1
+        elif nums[i] == 2:
+            nums[i], nums[right] = nums[right], nums[i]
+            right -= 1
+            i -= 1
+        i += 1
     print(nums)
 
 

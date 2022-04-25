a4cd4cb0e10fffdaf3fbacc29d69b4707025b3c2
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-24 21:02:59 -0500

    Add cleaner, significantly more performant version

--- medium/arrays/colors.py
+++ medium/arrays/colors.py
@@ -4,11 +4,7 @@ def sort_colors(nums: List[int]) -> None:
     """
      Do not return anything, modify nums in-place instead.
     """
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] > nums[j]:
-                nums[i], nums[j] = nums[j], nums[i]
-    
+    nums.sort()
     print(nums)
 
 

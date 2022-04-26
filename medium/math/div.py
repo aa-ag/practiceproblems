1e40802831817447ce46fd70f3e6c82f52055114
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-25 21:25:06 -0500

    Add two test cases

--- medium/math/div.py
+++ medium/math/div.py
@@ -1,4 +1,13 @@
 
 
 def divide(dividend: int, divisor: int) -> int:
-    pass
+    return dividend, divisor
+
+
+# Input: dividend = 10, divisor = 3
+# Output: 3
+print(divide(10, 3))
+
+# Input: dividend = 7, divisor = -3
+# Output: -2
+print(divide(7, -3))

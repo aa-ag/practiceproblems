f99a38921acb80ded8b347990014cb83c3ceaa09
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-28 21:11:06 -0500

    Add two test cases

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -2,3 +2,13 @@ from typing import List
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
     pass
+
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+print(top_k_frequent([1,1,1,2,2,3], 2))
+
+
+# Input: nums = [1], k = 1
+# Output: [1]
+print(top_k_frequent([1], 1))

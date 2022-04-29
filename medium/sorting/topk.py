ae7a0bdce31d1e52a74f0bcfb1880473cbcc44fb
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-28 21:17:54 -0500

    Add logic to iterate over dict and form an answer list

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -2,7 +2,16 @@ from typing import List
 from collections import Counter as c
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
-    return c(nums)
+    answer = list()
+
+    i = 0
+
+    while i < k:
+        for k, v in c(nums).items():
+            answer.append(k)
+            i += 1
+
+    return answer
 
 # Input: nums = [1,1,1,2,2,3], k = 2
 # Output: [1,2]

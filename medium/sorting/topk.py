0902d4cb953e55f4ea59a2eaa94ae1bd5aee115c
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-28 21:29:00 -0500

    Add thrid test case, failing in LeetCode

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -8,7 +8,10 @@ def top_k_frequent(nums: List[int], k: int) -> List[int]:
 # Output: [1,2]
 print(top_k_frequent([1,1,1,2,2,3], 2))
 
-
 # Input: nums = [1], k = 1
 # Output: [1]
 print(top_k_frequent([1], 1))
+
+# Input: [3,0,1,0] 
+# Output: [0]
+print(top_k_frequent([3,0,1,0], 1))

ba89e549fc0b630b1298dbe8ebeeb8c405999ceb
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-28 21:26:51 -0500

    Add one liner version slicing list of keys from the Counter object

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -2,16 +2,7 @@ from typing import List
 from collections import Counter as c
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
-    answer = list()
-
-    i = 0
-
-    while i < k:
-        for k, v in c(nums).items():
-            answer.append(k)
-            i += 1
-
-    return answer
+    return list(c(nums).keys())[:k]
 
 # Input: nums = [1,1,1,2,2,3], k = 2
 # Output: [1,2]

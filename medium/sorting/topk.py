5691364f64afb20b609a08431c54d42ee78f45b6
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-30 23:44:06 -0500

    Add option with operator, not really what i need since this converts a dict into a list of ordered tuples

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -1,8 +1,11 @@
+from ast import operator
 from typing import List
 from collections import Counter as c
+import operator
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
-    return list(c(nums).keys())[:k]
+    d = c(nums)
+    return sorted(d.items(), key=operator.itemgetter(1))
 
 # Input: nums = [1,1,1,2,2,3], k = 2
 # Output: [1,2]

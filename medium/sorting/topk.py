987a58f1c860b69c87011510d9f7ec0299a881db
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-30 23:56:35 -0500

    Add long but effective logic to sort dictionary by values

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -3,16 +3,17 @@ from typing import List
 from collections import Counter as c
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
-    keys = list(c(nums).keys())[:k]
-    return keys
+    return {key: value for key, value in sorted(
+        c(nums).items(), key=lambda item: item[1], reverse=True
+    )}
 
 # Input: nums = [1,1,1,2,2,3], k = 2
 # Output: [1,2]
-print(top_k_frequent([1,1,1,2,2,3], 2))
+# print(top_k_frequent([1,1,1,2,2,3], 2))
 
 # Input: nums = [1], k = 1
 # Output: [1]
-print(top_k_frequent([1], 1))
+# print(top_k_frequent([1], 1))
 
 # Input: [3,0,1,0] 
 # Output: [0]

592ed206b3888f566f493e347ebff33fbaffdac6
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-30 23:53:12 -0500

    Version still failing with test case # three, as it does not recognize 0 as a value to count

--- medium/sorting/topk.py
+++ medium/sorting/topk.py
@@ -3,8 +3,8 @@ from typing import List
 from collections import Counter as c
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
-    d = c(nums)
-    return d.most_common(k)[0][0]
+    keys = list(c(nums).keys())[:k]
+    return keys
 
 # Input: nums = [1,1,1,2,2,3], k = 2
 # Output: [1,2]

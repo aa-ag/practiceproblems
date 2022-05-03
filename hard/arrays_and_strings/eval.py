2d94f38f04c7e45f34bc7bd45cb19017c04528bf
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-03 17:52:53 -0500

    Add three test cases

--- hard/arrays_and_strings/eval.py
+++ hard/arrays_and_strings/eval.py
@@ -2,3 +2,16 @@
 def calculate(s: str) -> int:
     pass
 
+
+
+# Input: s = "3+2*2"
+# Output: 7
+print(calculate("3+2*2"))
+
+# Input: s = " 3/2 "
+# Output: 1
+print(calculate(" 3/2 "))
+
+# Input: s = " 3+5 / 2 "
+# Output: 5
+print(calculate(" 3+5 / 2 "))

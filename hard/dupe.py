aec51e28304e9ec1175d921ed2039067596fbab4
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-21 19:33:59 -0500

    Add two test cases for find_duplicate

--- hard/dupe.py
+++ hard/dupe.py
@@ -5,3 +5,10 @@ from collections import Counter
 def find_duplicate(nums: List[int]) -> int:
     pass
         
+# Input: nums = [1,3,4,2,2]
+# Output: 2
+print(find_duplicate([1,3,4,2,2]))
+
+# Input: nums = [3,1,3,4,2]
+# Output: 3
+print(find_duplicate([3,1,3,4,2]))

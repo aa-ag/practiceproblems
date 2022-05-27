5bd9dcd30868b96b3dea26b0068e134272209dd6
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-26 21:10:55 -0500

    Add three test cases

--- medium/backtracking/board.py
+++ medium/backtracking/board.py
@@ -3,3 +3,17 @@ from typing import List
 
 def exist(board: List[List[str]], word: str) -> bool:
     pass
+
+
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+# Input: word = "ABCCED"
+# Output: true
+print(exist(board, "ABCCED"))
+
+# Input: word = "SEE"
+# Output: true
+print(exist(board, "SEE"))
+
+# Input: word = "ABCB"
+# Output: false
+print(exist(board, "ABCB"))

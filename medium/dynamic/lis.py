3bac5d712b725ac75119d312e7f3d6ff64e716d1
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-27 22:10:46 -0500

    Version still missing scenario where a number is left in the end of an array

--- medium/dynamic/lis.py
+++ medium/dynamic/lis.py
@@ -1,4 +1,29 @@
 from typing import List
 
 def l(nums: List[int]) -> int:
-    pass
+    answer = list()
+    temp = 0
+
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i + 1]:
+            temp = nums[i]
+            continue
+        else:
+            if nums[i] > nums[i + 1]:
+                temp = nums[i]
+                continue
+            else:
+                answer.append(nums[i])
+    return answer
+
+# Input: nums = [10,9,2,5,3,7,101,18]
+# Output: 4
+print(l([10,9,2,5,3,7,101,18]))
+
+# Input: nums = [0,1,0,3,2,3]
+# Output: 4
+# print(l([0,1,0,3,2,3]))
+
+# Input: nums = [7,7,7,7,7,7,7]
+# Output: 1
+# print(l([7,7,7,7,7,7,7]))

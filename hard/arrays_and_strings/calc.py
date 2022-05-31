b2bb041ae8ea7b99dc33d48a25fe50f0e756eb13
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-30 20:54:44 -0500

    Return eval sotlution

--- hard/arrays_and_strings/calc.py
+++ hard/arrays_and_strings/calc.py
@@ -1,18 +1,18 @@
 
 def calculate(s: str) -> int:
-    pass
+    return eval(s)
 
 
 # Input: s = "3+2*2"
 # Output: 7
-print(calculate())
+print(calculate("3+2*2"))
 
 # Input: s = " 3/2 "
 # Output: 1
-print(calculate())
+print(calculate(" 3/2 "))
 
 # Input: s = " 3+5 / 2 "
 # Output: 5
-print(calculate())
+print(calculate(" 3+5 / 2 "))
 
         

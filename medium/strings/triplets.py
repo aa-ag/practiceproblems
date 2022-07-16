5f173fed068bf9d57498ec7ea81afe0ef0e16ad5
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-15 21:56:10 -0500

    More performant solution with helper function

--- medium/strings/triplets.py
+++ medium/strings/triplets.py
@@ -2,28 +2,35 @@ import enum
 from typing import List
 
 def three_sum(nums: List[int]) -> List[List[int]]:
-    res = []
-    nums.sort()
+        triples = []
+        
+        nums.sort()
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i] != nums[i-1]:
+                triples.extend(twoSum(nums, i))
+        return triples
     
-    for i,n in enumerate(nums):
-        
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
-        
-        l , r = i + 1, len(nums) - 1
-        
-        while l >= 0 and r < len(nums) and l < r:
-            if nums[i] + nums[l] + nums[r] < 0:
-                l += 1
-            elif nums[i] + nums[l] + nums[r] > 0:
-                r -= 1
-            else:
-                
-                res.append([nums[i] , nums[l] , nums[r]])
-                l += 1
-                while (l > 0 and nums[l] == nums[l - 1] and l < r):
-                    l += 1
-    return res
+def twoSum(nums: List[int], i: int) -> List[List[int]]:
+    triples = []
+    
+    j, k = i + 1, len(nums) - 1
+    while j < k:
+        total = nums[i] + nums[j] + nums[k]
+        if total == 0:
+            triples.append([nums[i], nums[j], nums[k]])
+            j += 1
+            k -= 1
+            while j < k and nums[k] == nums[k+1]:
+                k -= 1
+        elif total < 0:
+            j += 1
+        else:
+            k -= 1
+            
+    return triples
 
 # Input: nums = [-1,0,1,2,-1,-4]
 # Output: [[-1,-1,2],[-1,0,1]]

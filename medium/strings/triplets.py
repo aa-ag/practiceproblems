9ae39edb044b184b65b756888080a8d7ab70427b
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-15 21:53:53 -0500

    solution by soumyadexter7 found in the comments

--- medium/strings/triplets.py
+++ medium/strings/triplets.py
@@ -1,7 +1,29 @@
+import enum
 from typing import List
 
 def three_sum(nums: List[int]) -> List[List[int]]:
-    pass
+    res = []
+    nums.sort()
+    
+    for i,n in enumerate(nums):
+        
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        
+        l , r = i + 1, len(nums) - 1
+        
+        while l >= 0 and r < len(nums) and l < r:
+            if nums[i] + nums[l] + nums[r] < 0:
+                l += 1
+            elif nums[i] + nums[l] + nums[r] > 0:
+                r -= 1
+            else:
+                
+                res.append([nums[i] , nums[l] , nums[r]])
+                l += 1
+                while (l > 0 and nums[l] == nums[l - 1] and l < r):
+                    l += 1
+    return res
 
 # Input: nums = [-1,0,1,2,-1,-4]
 # Output: [[-1,-1,2],[-1,0,1]]

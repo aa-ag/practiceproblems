1d4d1213e17f25fe6525ee821bd7d9fc1007c5c3
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-15 21:45:11 -0500

    Add three sample cases for testing

--- medium/strings/triplets.py
+++ medium/strings/triplets.py
@@ -2,3 +2,15 @@ from typing import List
 
 def three_sum(nums: List[int]) -> List[List[int]]:
     pass
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+print(three_sum([-1,0,1,2,-1,-4]))
+
+# Input: nums = [0,1,1]
+# Output: []
+print(three_sum([0,1,1]))
+
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+print(three_sum([0,0,0]))

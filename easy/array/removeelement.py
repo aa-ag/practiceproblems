4e9ab30fa58be1d1d100d41149140039796ab51e
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-24 12:57:09 -0500

    Add two test cases

--- easy/array/removeelement.py
+++ easy/array/removeelement.py
@@ -5,3 +5,10 @@ def remove_element(nums: List[int], val: int) -> int:
     pass
 
 
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2,_,_]
+print(remove_element([3,2,2,3],3))
+
+# Input: [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+print(remove_element([0,1,2,2,3,0,4,2],2))

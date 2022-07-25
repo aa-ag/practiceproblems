158a36f8fa065df796fd2a4cce4989f6c226fbb6
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-25 18:56:03 -0500

    Add three test cases from leetcode

--- easy/array/sip.py
+++ easy/array/sip.py
@@ -2,3 +2,16 @@ from typing import List
 
 def sip(nums: List[int], target: int) -> int:
     pass
+
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+print(sip([1,3,5,6], 5))
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+print(sip([1,3,5,6], 2))
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+print(sip([1,3,5,6], 7))

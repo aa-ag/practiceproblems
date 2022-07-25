9306e47e09bc5d13f06a02964f444e24e57e85f1
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-25 18:49:38 -0500

    Add three test cases

--- easy/strings/lolw.py
+++ easy/strings/lolw.py
@@ -1,3 +1,16 @@
 
 def lolw(s: str) -> int:
     pass
+
+
+# Input: s = "Hello World"
+# Output: 5
+print(lolw("Hello World"))
+
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+print(lolw("   fly me   to   the moon  "))
+
+# Input: s = "luffy is still joyboy"
+# Output: 6
+print(lolw("luffy is still joyboy"))

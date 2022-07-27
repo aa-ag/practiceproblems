58581e78bfa415973d1dd64637db822f367c05d8
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-26 20:42:01 -0500

    Went back and re-read this problems instructions, was able to provide a solution that's faster than 70% of solutions, and uses less memory than 80% of submissions

--- easy/array/sip.py
+++ easy/array/sip.py
@@ -4,25 +4,23 @@ def sip(nums: List[int], target: int) -> int:
     if target in nums:
         return nums.index(target)
     else:
-        l = list(i for i in range(1, max(nums)+1))
-        if target > max(l):
-            return len(nums)
-        elif target > min(l):
-            return 0
-        else:
-            return l.index(target)
-            
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
             
 
+# [1,3,5]
+# 4
+print(sip([1,3,5], 4))
 
 # Input: nums = [1,3,5,6], target = 5
 # Output: 2
-print(sip([1,3,5,6], 5))
+# print(sip([1,3,5,6], 5))
 
 # Input: nums = [1,3,5,6], target = 2
 # Output: 1
-print(sip([1,3,5,6], 2))
+# print(sip([1,3,5,6], 2))
 
 # Input: nums = [1,3,5,6], target = 7
 # Output: 4
-print(sip([1,3,5,6], 7))
+# print(sip([1,3,5,6], 7))

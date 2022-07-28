d4b95e5d4288417a4d932a7543dc9d1497422020
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-27 22:22:09 -0500

    Add two test scenarios from leetcode

--- easy/math/addbin.py
+++ easy/math/addbin.py
@@ -1,2 +1,10 @@
 def add_binary(a: str, b: str) -> str:
-    pass
+    return a + b
+
+# Input: a = "11", b = "1"
+# Output: "100"
+print(add_binary("11", "1"))
+
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+print(add_binary("1010", "1011"))

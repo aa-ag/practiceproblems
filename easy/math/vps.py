4c7853227eb88ebc303408fbcc6591dbf7ea806f
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-28 20:31:08 -0500

    Add two test cases

--- easy/math/vps.py
+++ easy/math/vps.py
@@ -4,3 +4,11 @@ def vps(num: int) -> bool:
      check if an integer is a valid perfect square
     '''
     pass
+
+# Input: num = 16
+# Output: true
+print(vps(16))
+
+# Input: num = 14
+# Output: false
+print(vps(14))

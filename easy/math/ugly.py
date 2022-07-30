d2b2278094b1f673a64c2ff0598c9261566ddadf
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-30 17:40:11 -0500

    Add three test cases

--- easy/math/ugly.py
+++ easy/math/ugly.py
@@ -3,3 +3,14 @@ def is_ugly(n: int) -> bool:
     pass
 
 
+# Input: n = 6
+# Output: true
+print(is_ugly(6))
+
+# Input: n = 1
+# Output: true
+print(is_ugly(1))
+
+# Input: n = 14
+# Output: false
+print(is_ugly(14))

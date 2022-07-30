6160286b2c770f7dcb6a8668f4a2911ee3215383
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-29 20:19:31 -0500

    Add three test cases

--- easy/math/addstrings.py
+++ easy/math/addstrings.py
@@ -2,3 +2,14 @@ def add_strings(num1: str, num2: str) -> str:
     pass
 
 
+# Input: num1 = "11", num2 = "123"
+# Output: "134"
+print(add_strings("11", "123"))
+
+# Input: num1 = "456", num2 = "77"
+# Output: "533"
+print(add_strings("456", "77"))
+
+# Input: num1 = "0", num2 = "0"
+# Output: "0"
+print(add_strings("0", "0"))

44d2a4bdfecbe745b6b8918deebea8de2d19bde7
aa-ag <aaron.aguerrevere@gmail.com>
2022-07-31 17:43:18 -0500

    Add two test cases

--- easy/strings/segments.py
+++ easy/strings/segments.py
@@ -1,3 +1,11 @@
 def countSegments(s: str) -> int:
     pass
 
+
+# Input: s = "Hello, my name is John"
+# Output: 5
+print(countSegments("Hello, my name is John"))
+
+# Input: s = "Hello"
+# Output: 1
+print(countSegments("Hello"))

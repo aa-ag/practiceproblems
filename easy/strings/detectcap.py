b5140c58feb10a6c6b00de1646101e56a785d7dd
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-01 22:51:04 -0500

    Add test cases

--- easy/strings/detectcap.py
+++ easy/strings/detectcap.py
@@ -1,2 +1,10 @@
-def detectCapitalUse(word: str) -> bool:
+def dcu(word: str) -> bool:
     pass
+
+# Input: word = "USA"
+# Output: true
+print(dcu())
+
+# Input: word = "FlaG"
+# Output: false
+print(dcu())

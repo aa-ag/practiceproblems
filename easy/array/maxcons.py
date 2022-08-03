557d76c638d0c0b7e46e3af1056163c93f6566f0
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-02 20:04:29 -0500

    Add two test cases

--- easy/array/maxcons.py
+++ easy/array/maxcons.py
@@ -3,3 +3,10 @@ from typing import List
 def fmx(nums: List[int]) -> int:
     pass
 
+# Input: nums = [1,1,0,1,1,1]
+# Output: 3
+print(fmx([1,1,0,1,1,1]))
+
+# Input: nums = [1,0,1,1,0,1]
+# Output: 2
+print(fmx([1,0,1,1,0,1]))

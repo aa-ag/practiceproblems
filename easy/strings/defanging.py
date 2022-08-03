663480443670a5d61d80b9afb0e9aefc0d18f74d
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-03 18:01:41 -0500

    Add two test cases

--- easy/strings/defanging.py
+++ easy/strings/defanging.py
@@ -1,2 +1,12 @@
 def defang(address: str) -> str:
     pass
+
+
+
+# Input: address = "1.1.1.1"
+# Output: "1[.]1[.]1[.]1"
+print(defang("1.1.1.1"))
+
+# Input: address = "255.100.50.0"
+# Output: "255[.]100[.]50[.]0"
+print(defang("255.100.50.0"))

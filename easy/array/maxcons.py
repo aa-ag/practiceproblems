d6b5ae7570a3d72c2ce33e155634f27b679703c2
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-02 20:18:31 -0500

    Add temporary solution

--- easy/array/maxcons.py
+++ easy/array/maxcons.py
@@ -1,7 +1,18 @@
 from typing import List
 
 def fmx(nums: List[int]) -> int:
-    pass
+    groups = list()
+    temp = 0
+
+    for num in nums:
+        if num == 1:
+            temp += 1
+        else:
+            groups.append(temp)
+            temp = 0
+    groups.append(temp)
+    return max(groups)
+
 
 # Input: nums = [1,1,0,1,1,1]
 # Output: 3

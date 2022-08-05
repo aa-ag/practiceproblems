fc48f3c4e433206461c591b788a6f11b67c903d7
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-04 22:45:57 -0500

    Add mean formula at return with new lenght of arr after taking five_percent from each end

--- easy/math/trimmean.py
+++ easy/math/trimmean.py
@@ -5,7 +5,7 @@ def tm(arr: List[int]) -> float:
     five_percent = int(l * 0.05)
     arr.sort()
     arr =  arr[five_percent:-five_percent]
-    return arr
+    return sum(arr) / len(arr)
 
 # Input: arr = [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3]
 # Output: 2.00000

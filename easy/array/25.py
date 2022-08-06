7afa6b3d896e24fb5d62200ba5362feb24f2373a
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-05 19:50:19 -0500

    Add formula for 25%

--- easy/array/25.py
+++ easy/array/25.py
@@ -1,4 +1,14 @@
 from typing import List
 
-def findSpecialInteger(arr: List[int]) -> int:
-    pass
+def fsc(arr: List[int]) -> int:
+    return round(len(arr) * 0.25)
+
+
+# Input: arr = [1,2,2,6,6,6,6,7,10]
+# Output: 6
+
+print(fsc([1,2,2,6,6,6,6,7,10]))
+
+# Input: arr = [1,1]
+# Output: 1
+print(fsc([1,1]))

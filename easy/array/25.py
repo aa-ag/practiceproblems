d705a4189d3bccd00e8b9b53af9664a4d035611f
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-05 19:53:07 -0500

    Add third test case

--- easy/array/25.py
+++ easy/array/25.py
@@ -10,9 +10,12 @@ def fsc(arr: List[int]) -> int:
 
 # Input: arr = [1,2,2,6,6,6,6,7,10]
 # Output: 6
-
 print(fsc([1,2,2,6,6,6,6,7,10]))
 
 # Input: arr = [1,1]
 # Output: 1
 print(fsc([1,1]))
+
+# Input: [1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]
+# Output: None
+print(fsc([1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]))

4ce42c55c7a41469cce0b6f1a209bb684ac574d2
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-08 23:07:37 -0500

    Add answer and partial answer algo

--- easy/strings/phones.py
+++ easy/strings/phones.py
@@ -2,7 +2,20 @@ import re
 
 def reformat(number: str) -> str:
     digits = ''.join(i for i in number if i.isdigit())
-    return digits
+
+    answer = ''
+
+    while len(digits):
+        if len(digits) > 4:
+            chunk = digits[:3]
+            answer += chunk + '-'
+            digits = digits[3:]
+        else:
+            chunk = digits[:2]
+            answer += chunk + '-'
+            digits = digits[2:]
+
+    return answer
 
 
 # Input: number = "1-23-45 6"

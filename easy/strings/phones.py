1d062c4c1013261f50a937cdd249bc63c9e615a4
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-08 22:56:17 -0500

    Add three test cases

--- easy/strings/phones.py
+++ easy/strings/phones.py
@@ -1,2 +1,15 @@
 def reformat(number: str) -> str:
     pass
+
+
+# Input: number = "1-23-45 6"
+# Output: "123-456"
+print(reformat("1-23-45 6"))
+
+# Input: number = "123 4-567"
+# Output: "123-45-67"
+print(reformat("123 4-567"))
+
+# Input: number = "123 4-5678"
+# Output: "123-456-78
+print(reformat("123 4-5678"))

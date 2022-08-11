893f786cbb4d385b086f90ceaf30781e64165cda
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-10 21:44:11 -0500

    Add two test cases

--- medium/math/additive.py
+++ medium/math/additive.py
@@ -2,10 +2,10 @@ def is_it(num: str) -> bool:
     pass
 
 
-print(is_it())
+# Input: "112358"
+# Output: true
+print(is_it(112358))
 
-
-print(is_it())
-
-
-print(is_it())
+# Input: "199100199"
+# Output: true
+print(is_it(199100199))

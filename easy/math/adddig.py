c582f703efba9bc09d3ce90a2a81cd53d22715c3
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-15 21:14:04 -0500

    Add check to see if the length of num as a string equals 1 to return num if that's the case

--- easy/math/adddig.py
+++ easy/math/adddig.py
@@ -1,5 +1,14 @@
 def add_dig(num: int) -> int:
-    pass
+    n = len(str(num))
+
+    if n == 1:
+        return num
+    while n > 1:
+        l = [int(i) for i in str(n)]
+        print(l)
+        n = len(str(l))
+
+    return n
 
 # Input: num = 38
 # Output: 2

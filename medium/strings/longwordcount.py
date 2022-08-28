0c3e5c002c0beb6554d5090040e1080f0bb80179
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-28 17:10:59 -0500

    Add two test cases

--- medium/strings/longwordcount.py
+++ medium/strings/longwordcount.py
@@ -2,3 +2,15 @@ from typing import List
 
 def lwc(messages: List[str], senders: List[str]) -> str:
     pass
+
+
+# Output: "Alice"
+messages = ["Hello userTwooo","Hi userThree","Wonderful day Alice","Nice day userThree"]
+senders = ["Alice","userTwo","userThree","Alice"]
+print(lwc(messages, senders))
+
+
+# Output: "Charlie"
+messages = ["How is leetcode for everyone","Leetcode is useful for practice"]
+senders = ["Bob","Charlie"]
+print(lwc(messages, senders))

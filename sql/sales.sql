d4001e4da65cf29e7e32fb40ad60e69d362d8e36
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-20 20:54:52 -0600

    Add answer to 1211

--- sql/sales.sql
+++ sql/sales.sql
@@ -8,3 +8,11 @@ GROUP BY p.product_id;
 SELECT product_id, sum(quantity) as total_quantity
 FROM sales
 GROUP BY product_id
+
+-- 1211. Queries Quality and Percentage
+SELECT
+    query_name,
+    ROUND(SUM((rating / position)) / COUNT(query_name),2) quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(query_name) * 100,2) poor_query_percentage
+FROM Queries
+GROUP BY query_name;

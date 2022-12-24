f1af703604c8accf829dea4cfd6fc06fab1b61ea
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-23 19:38:09 -0500

    Accepted answer

--- sql/1241.sql
+++ sql/1241.sql
@@ -1,7 +1,7 @@
 -- 1241. Number of Comments per Post
-SELECT
-    DISTINCT sub_id post_id,
-    parent_id number_of_comments
-FROM Submissions
-WHERE parent_id IS NULL
+SELECT a.sub_id post_id,
+(SELECT COUNT(DISTINCT(b.sub_id)) FROM Submissions b WHERE b.parent_id=a.sub_id) number_of_comments
+FROM Submissions a
+WHERE a.parent_id IS NULL
+GROUP BY a.sub_id
 ORDER BY post_id ASC;

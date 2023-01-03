ba1a00d2632f422e44bb7c6feeee386ea091343e
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-02 19:01:31 -0600

    Add ORDER BY to accepted answer

--- sql/1607.sql
+++ sql/1607.sql
@@ -1,6 +1,7 @@
-SELECT seller_id
+SELECT seller_name
 FROM Seller
 WHERE seller_id NOT IN (
     SELECT seller_id
     FROM Orders
-    WHERE YEAR(sale_date) = '2020');
+    WHERE YEAR(sale_date) = '2020')
+ORDER BY seller_name ASC;

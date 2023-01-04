549b7f4dc0e2d06e2fd6557a416cfd3f0c3cbddb
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-03 21:07:06 -0600

    Swap customer_count for order_count, and add count of order_id's, accepted answer

--- sql/1565.sql
+++ sql/1565.sql
@@ -1,6 +1,7 @@
 SELECT
     DATE_FORMAT(order_date, '%Y-%m') month,
-    COUNT(DISTINCT customer_id) order_count
+    COUNT(order_id) order_count,
+    COUNT(DISTINCT customer_id) customer_count
 FROM Orders
 WHERE invoice > 20
 GROUP BY month;

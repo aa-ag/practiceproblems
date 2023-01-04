8461698f30a3c77f0fee808b1a7361aa2638e39e
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-03 21:02:34 -0600

    Add order_count

--- sql/1565.sql
+++ sql/1565.sql
@@ -1,3 +1,5 @@
 SELECT
-    DATE_FORMAT(order_date, '%Y-%m') month
-FROM Orders;
+    DATE_FORMAT(order_date, '%Y-%m') month,
+    COUNT(DISTINCT customer_id) order_count
+FROM Orders
+GROUP BY month;

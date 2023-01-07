f138205b96298cb5a07c2fe1202bdc9361c70963
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-06 19:32:03 -0600

    Add outerquery WHERE IN inner query

--- sql/184.sql
+++ sql/184.sql
@@ -1,8 +1,9 @@
-SELECTd.name, e.name, e.salary
+SELECT d.name, e.name, e.salary
 FROM Department d
 JOIN Employee e
-ON d.id=e.departmentId;
-
-SELECT departmentId, MAX(salary) m
-FROM Employee
-GROUP BY departmentId;
+ON d.id=e.departmentId
+WHERE (d.id,e.salary) IN (
+    SELECT departmentId, MAX(salary) m
+    FROM Employee
+    GROUP BY departmentId
+);

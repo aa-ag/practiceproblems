40ddcc1faffb41dc733cd6c43ed453f0332f7ecd
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-08 18:04:56 -0600

    Add outer query to accepted answer

--- sql/1831.sql
+++ sql/1831.sql
@@ -1,5 +1,11 @@
 SELECT
-    DATE(day) transaction_date,
-    MAX(amount)
+    transaction_id
 FROM Transactions
-GROUP BY transaction_date
+WHERE (DATE(day), amount) IN (
+    SELECT
+        DATE(day),
+        MAX(amount)
+    FROM Transactions
+    GROUP BY DATE(day)
+)
+ORDER BY transaction_id ASC;

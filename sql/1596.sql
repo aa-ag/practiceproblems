0c2c02e5dc799814d3d5a5985749a9d39d4b0629
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-09 20:22:46 -0600

    Add cte

--- sql/1596.sql
+++ sql/1596.sql
@@ -5,3 +5,6 @@ WITH cte as (
     FROM Orders
     GROUP BY customer_id, product_id
 )
+SELECT cte.customer_id, p.product_id, p.product_name
+FROM cte, Products as p
+WHERE cte.product_id=p.product_id AND cte.o_rank=1;

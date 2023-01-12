53276d108c3476191e17497034e42d4742a209db
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-11 21:26:53 -0600

    Add alias

--- sql/1241.sql
+++ sql/1241.sql
@@ -4,6 +4,6 @@ SELECT
         COUNT(DISTINCT b.sub_id)
         FROM Submissions b 
         WHERE a.sub_id=b.parent_id
-    )
+    ) number_of_comments
 FROM Submissions a
 WHERE a.parent_id IS NULL;

7a9231cf51640dbdeea1c357bcbe4ca5cf342410
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-11 21:26:18 -0600

    Refactor number_of_comments

--- sql/1241.sql
+++ sql/1241.sql
@@ -1,7 +1,9 @@
--- 1241. Number of Comments per Post
-SELECT a.sub_id post_id,
-(SELECT COUNT(DISTINCT(b.sub_id)) FROM Submissions b WHERE b.parent_id=a.sub_id) number_of_comments
+SELECT
+    DISTINCT a.sub_id post_id,
+    (SELECT 
+        COUNT(DISTINCT b.sub_id)
+        FROM Submissions b 
+        WHERE a.sub_id=b.parent_id
+    )
 FROM Submissions a
-WHERE a.parent_id IS NULL
-GROUP BY a.sub_id
-ORDER BY post_id ASC;
+WHERE a.parent_id IS NULL;

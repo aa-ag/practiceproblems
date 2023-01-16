750d88352252fb7ba0fd0d6773ec038209fa8b0f
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-15 18:29:01 -0600

    Add accpeted answer from Solutions

--- sql/1613.sql
+++ sql/1613.sql
@@ -1,8 +1,11 @@
-WITH RECURSIVE seq AS (
-    SELECT 0 AS rng
-    UNION ALL SELECT rng + 1
-    FROM seq
-    WHERE rng < 100
+WITH RECURSIVE CTE AS(
+    SELECT 1 AS Id,MAX(customer_id) AS end_id
+    FROM Customers
+    UNION ALL
+    SELECT Id+1,end_id
+    FROM CTE
+    WHERE CTE.Id<end_id
 )
 
-SELECT * FROM seq;
+SELECT Id AS ids FROM CTE
+WHERE Id NOT IN (SELECT customer_id from Customers)

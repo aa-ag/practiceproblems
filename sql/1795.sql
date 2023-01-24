972e179c6f9e782eba2eea9fc0e0b0b4983e6890
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-23 21:07:24 -0600

    Fix typo on output column names

--- sql/1795.sql
+++ sql/1795.sql
@@ -1,12 +1,12 @@
 SELECT
-    product_id,'store_1' AS store,store1 AS price FROM Products WHERE store1
+    product_id,'store1' AS store,store1 AS price FROM Products WHERE store1
 IS NOT NULL
 UNION
 SELECT
-    product_id,'store_2' AS store,store2 AS price FROM Products WHERE store2
+    product_id,'store2' AS store,store2 AS price FROM Products WHERE store2
 IS NOT NULL
 UNION
 SELECT
-    product_id,'store_3' AS store,store3 AS price FROM Products WHERE store3
+    product_id,'store3' AS store,store3 AS price FROM Products WHERE store3
 IS NOT NULL
 GROUP BY 1,2;

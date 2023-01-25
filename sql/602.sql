056578520b3e570bfb120c8326dfd6156aec717a
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-24 22:21:29 -0600

    Add distincts from UNION of subqueried tables

--- sql/602.sql
+++ sql/602.sql
@@ -1,9 +1,14 @@
-SELECT 
-    requester_id AS id,
-    accepter_id AS friend
-FROM RequestAccepted
-UNION
-SELECT 
-    accepter_id AS id,
-    requester_id  AS friend
-FROM RequestAccepted;
+SELECT
+    DISTINCT id,
+    COUNT(DISTINCT friend) friend_count
+FROM
+    (SELECT 
+        requester_id AS id,
+        accepter_id AS friend
+    FROM RequestAccepted
+    UNION
+    SELECT 
+        accepter_id AS id,
+        requester_id  AS friend
+    FROM RequestAccepted) t
+GROUP BY id;

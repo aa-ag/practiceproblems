b4d68a7f375e33dbbc2ae67a55bcfafe96073a2c
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-07 20:15:06 -0600

    Add opposite version

--- hackerrank/basicSelect.sql
+++ hackerrank/basicSelect.sql
@@ -50,3 +50,8 @@ SELECT
 FROM STATION
 WHERE LOWER(LEFT(CITY,1)) IN ('a','e','i','o','u')
 AND LOWER(RIGHT(CITY,1)) IN ('a','e','i','o','u');
+
+SELECT
+    DISTINCT CITY
+FROM STATION
+WHERE LOWER(LEFT(CITY,1)) NOT IN ('a','e','i','o','u');

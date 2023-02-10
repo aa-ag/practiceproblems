fa3be17310cd9a40113af84dfb1bd1713356ca7a
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-10 17:15:38 -0600

    Add WOS19 accepted answer

--- hackerrank/aggregations.sql
+++ hackerrank/aggregations.sql
@@ -78,3 +78,12 @@ SELECT
         ),4
     )
 FROM STATION;
+
+SELECT
+    ROUND(
+        SQRT(
+            POWER(ABS(MAX(LONG_W) - MIN(LONG_W)),2)
+            + 
+            POWER(ABS(MAX(LAT_N) - MIN(LAT_N)),2)
+        ),4)
+FROM STATION;

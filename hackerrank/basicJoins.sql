629e327110caab4c1e8ac1da607d8c0b4ea423f2
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-12 14:59:17 -0600

    Add Top Competitors base join logic, to be filtered

--- hackerrank/basicJoins.sql
+++ hackerrank/basicJoins.sql
@@ -20,3 +20,18 @@ FROM COUNTRY co
 JOIN CITY ci
 ON ci.COUNTRYCODE=co.CODE
 GROUP BY co.CONTINENT;
+
+SELECT 
+    s.hacker_id,
+    h.name,
+    s.score,
+    s.score,
+    c.difficulty_level,
+    d.score
+FROM Submissions s
+JOIN Hackers h 
+ON s.hacker_id=h.hacker_id
+JOIN Challenges c
+ON c.challenge_id=s.challenge_id
+JOIN Difficulty d
+ON d.difficulty_level=c.difficulty_level;

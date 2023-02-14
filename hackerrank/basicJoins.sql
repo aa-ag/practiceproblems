0913c5c6f4065e8805ce120bf65d096bc70c147c
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-13 19:29:52 -0600

    Add select name & group by on Top Competitors

--- hackerrank/basicJoins.sql
+++ hackerrank/basicJoins.sql
@@ -22,17 +22,12 @@ ON ci.COUNTRYCODE=co.CODE
 GROUP BY co.CONTINENT;
 
 SELECT 
-    s.hacker_id,
-    h.name,
-    s.score,
-    s.score,
-    c.difficulty_level,
-    d.score
+    DISTINCT s.hacker_id,
+    (SELECT h.name FROM HACKERS WHERE s.hacker_id=h.hacker_id)
 FROM Submissions s
-JOIN Hackers h 
-ON s.hacker_id=h.hacker_id
 JOIN Challenges c
 ON c.challenge_id=s.challenge_id
 JOIN Difficulty d
 ON d.difficulty_level=c.difficulty_level
-WHERE s.score=d.score;
+WHERE s.score=d.score
+GROUP BY s.hacker_id;

ced571bade699ee920a56c13258151f8b800177d
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-14 21:42:05 -0600

    Add order by id also in case of count tie

--- hackerrank/basicJoins.sql
+++ hackerrank/basicJoins.sql
@@ -32,4 +32,4 @@ ON d.difficulty_level=c.difficulty_level
 WHERE s.score=d.score
 GROUP BY s.hacker_id, h.name
 HAVING COUNT(*) > 1
-ORDER BY COUNT(*) DESC;
+ORDER BY COUNT(*) DESC, s.hacker_id ASC;

f6e1dc4426f64beed954c502ab0cd515fd9f6985
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-26 17:43:54 -0600

    Add Join logic

--- hackerrank/advancedSelect.sql
+++ hackerrank/advancedSelect.sql
@@ -1,3 +1,9 @@
 SELECT
-    DISTINCT c.company_code,c.founder
+    DISTINCT c.company_code,c.founder,l.lead_manager_code,s.senior_manager_code,e.employee_code
 FROM Company c
+JOIN Lead_Manager l
+ON c.company_code=l.company_code
+JOIN Senior_Manager s
+ON c.company_code=s.company_code
+JOIN Employee e
+ON c.company_code=e.company_code;

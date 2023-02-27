7fd39bb3f54532ed0b0b9a0e1a5dd2849dbf981e
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-26 18:04:23 -0600

    Refactor JOINs, accepted answer

--- hackerrank/advancedSelect.sql
+++ hackerrank/advancedSelect.sql
@@ -1,17 +1,12 @@
-SELECT
-    a.company_code company_code,
-    a.founder,
-    COUNT(a.lead_manager_code),
-    COUNT(a.senior_manager_code),
-    COUNT(a.employee_code),
-FROM (SELECT
-        DISTINCT c.company_code,c.founder,l.lead_manager_code,s.senior_manager_code,e.employee_code
-    FROM Company c
-    JOIN Lead_Manager l
-    ON c.company_code=l.company_code
-    JOIN Senior_Manager s
-    ON c.company_code=s.company_code
-    JOIN Employee e
-    ON c.company_code=e.company_code
-    ORDER BY c.company_code ASC) a
-GROUP BY company_code;
+SELECT 
+    c.company_code, 
+    c.founder, 
+    COUNT(DISTINCT e.lead_manager_code),
+    COUNT(DISTINCT e.senior_manager_code),
+    COUNT(DISTINCT e.manager_code),
+    COUNT(DISTINCT e.employee_code)
+FROM Company c
+JOIN Employee e
+ON c.company_code=e.company_code
+GROUP BY c.company_code,c.founder
+ORDER BY c.company_code;
